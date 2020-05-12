500cc65b0e6ea3230415f7f7937078ab8305d978
bobjiangps <jbsv43@sina.com>
2020-05-12 13:52:49 +0800

    add script to generate captcha, move debug file

---
+++ generate_captcha.py
@@ -0,0 +1,25 @@
+from captcha.image import ImageCaptcha
+import numpy as np
+import random
+import string
+import os
+
+
+def generate_save_captcha(length, amount, folder_name, width=130, height=70):
+    characters = string.digits + string.ascii_lowercase
+    generator = ImageCaptcha(width=width, height=height)
+    img_path = os.path.join(os.getcwd(), "img", folder_name)
+    if not os.path.exists(img_path):
+        os.makedirs(img_path)
+    for _ in range(amount):
+        captcha_str = "".join([random.choice(characters) for j in range(length)])
+        img = generator.create_captcha_image(captcha_str, (65, 105, 225), (255, 255, 255))
+        img.save(os.path.join(img_path, "%s.png" % captcha_str))
+
+
+if __name__ == "__main__":
+    train_img_amount = 60000
+    test_img_amount = 10000
+    captcha_str_length = 4
+    generate_save_captcha(captcha_str_length, train_img_amount, "train")
+    generate_save_captcha(captcha_str_length, test_img_amount, "test")
